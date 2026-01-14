9424abd7acc4cfc5bf07d4abd7e36b2b5055714f
KaeTheDev <kaethedev@gmail.com>
2026-01-14 00:01:13 -0500

    added read product route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -13,3 +13,22 @@ router.post("/", async (req, res) => {
       });
     }
   });
+
+  // Read a single product
+  router.get("/:id", async (req, res) => {
+    try {
+      const product = await Product.findById(req.params.id);
+  
+      if (!product) {
+        return res.status(404).json({
+          message: "Product not found",
+        });
+      }
+  
+      res.json(product);
+    } catch (error) {
+      res.status(400).json({
+        error: "Invalid product ID",
+      });
+    }
+  });
